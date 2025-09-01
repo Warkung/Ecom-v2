fc964920cd245b5e5a36516f716726d20760ed5a
Warkung <warkung@gmail.com>
2025-09-01 14:14:48 +0700

    first commit branch

--- client/src/layouts/Layout.tsx
+++ client/src/layouts/Layout.tsx
@@ -1,15 +1,12 @@
 import { Outlet } from "react-router-dom";
 import MainNav from "../components/public/MainNav";
 import { navLinks } from "../utils/link";
-import { ModeToggle } from "../components/darkmode/mode-toggle";
-import { ThemeProvider } from "../components/darkmode/theme-provider";
 
 export default function Layout() {
   return (
     <div>
       <MainNav navLinks={navLinks} hidden={false} />
-      <div className="absolute top-20 right-4"></div>
-      <main className="container mx-auto p-4">
+      <main className="container mx-auto px-6 mt-6">
         <Outlet />
       </main>
     </div>
