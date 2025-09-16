93787fa5cfca5bacc69b6fa5253567c0f7885036
George <warkung@gmail.com>
2025-09-16 18:47:48 +0700

    before payment session

--- client/src/layouts/LayoutUser.tsx
+++ client/src/layouts/LayoutUser.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
+import MainNav from "../components/navbar/MainNav";
+import { navLinks } from "../utils/link";
 
 export default function LayoutUser() {
   return (
     <div>
-      <h1>Home User</h1>
-      <hr />
-      <Outlet />
+      <MainNav navLinks={navLinks} hidden={false} />
+      <main className="container mx-auto px-6 mt-6">
+        <Outlet />
+      </main>
     </div>
   );
 }
